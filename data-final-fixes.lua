68c01d911e289bbf65efa2ccee9fb82851504c35
Kubius <12454065+Kubius@users.noreply.github.com>
2024-11-28 11:02:15 -0600

    0.1.2

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -1,2 +1,4 @@
 require("__wayward-seas__/prototypes/world_gen")
 require("__wayward-seas__/prototypes/placement_adjust")
+
+table.insert(data.raw["assembling-machine"]["assembling-machine-1"].crafting_categories,"organic-or-hand-crafting")
