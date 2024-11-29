cc78d66e818a22e7b3c815bf969a92a5469c9e7c
Kubius <12454065+Kubius@users.noreply.github.com>
2024-11-29 11:05:41 -0600

    0.2.3

--- prototypes/world_gen.lua
+++ prototypes/world_gen.lua
@@ -19,6 +19,6 @@ data.raw["noise-expression"]["gleba_starting_area_multiplier"].expression = "0.2
 data.raw["noise-expression"]["gleba_starting_enemies"].expression = "0"
 data.raw["noise-expression"]["gleba_elevation_common"].local_expressions["ridges"] = "-(wayward_seas_gleba_prep + 0.9 + 0.4 * sqrt(var('control:gleba_water:size')))"
 data.raw["noise-expression"]["gleba_elevation_common"].local_expressions["ridge_terrace"] = "terrace{value = min(80, 110 + ridges * 1000), offset = 60, width = 60, strength = min(0.2,0.5 * var('control:gleba_water:size'))}"
-data.raw["noise-expression"]["gleba_stone_richness"].expression = "1000 * max(starting, gleba_simple_spot(1000, 12 * size ^ 0.5, 220 / frequency ^ 0.5, 1) * max(gleba_highland*5, gleba_midland_aux_2, wayward_seas_lowstone + 0.5 * (gleba_elevation_common < gleba_deep_water_level))) * richness / size"
+data.raw["noise-expression"]["gleba_stone_richness"].expression = "1000 * max(starting, gleba_simple_spot(1000, 12 * size ^ 0.5, 220 / frequency ^ 0.5, 1) * max(gleba_highland*5, gleba_midland_aux_2, wayward_seas_lowstone + wayward_seas_lowstone * 0.5 * (gleba_elevation_common < gleba_deep_water_level))) * richness / size"
 
 --data.raw["noise-expression"]["gleba_shallows"].expression = "gleba_select(gleba_elevation, -3.5, -2, 0.4, 0, 1)"
