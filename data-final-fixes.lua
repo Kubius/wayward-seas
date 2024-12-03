d0795d68c5120068183fb965f2ca0e42e04f5a3c
Kubius <12454065+Kubius@users.noreply.github.com>
2024-12-03 15:19:34 -0600

    0.4.1

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -1,4 +1,7 @@
-require("__wayward-seas__/prototypes/world_gen")
+
+if settings.startup["enable_ws_worldgen"].value then
+    require("__wayward-seas__/prototypes/world_gen")
+end
 require("__wayward-seas__/prototypes/placement_adjust")
 
 table.insert(data.raw["assembling-machine"]["assembling-machine-1"].crafting_categories,"organic-or-hand-crafting")
--- settings.lua
+++ settings.lua
@@ -4,8 +4,14 @@ data:extend{
         name = "world_gen_revision",
         setting_type = "startup",
         default_value = 2,
-        minimum_value = 1,
-        maximum_value = 2,
+        allowed_values = {1, 2},
         order = "c-a"
+    },
+    {
+        type = "bool-setting",
+        name = "enable_ws_worldgen",
+        setting_type = "startup",
+        default_value = true,
+        order = "c-b"
     },
 }
