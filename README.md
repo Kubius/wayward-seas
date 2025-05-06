4916366e6c5fd71f97e25aaec1a35ef344804635
Kubius <12454065+Kubius@users.noreply.github.com>
2025-05-06 17:17:32 -0500

    0.8.1

---
+++ README.md
@@ -0,0 +1,3 @@
+# On Wayward Seas
+
+A Factorio: Space Age mod overhauling the DLC planet Gleba into an archipelagic world, with simple-to-cultivate basic crops, new production chains and other features making for an easier introduction to the planet.
--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -4,6 +4,7 @@ end
 require("__wayward-seas__/prototypes/placement_adjust")
 
 table.insert(data.raw["assembling-machine"]["assembling-machine-1"].crafting_categories,"organic-or-hand-crafting")
+table.insert(data.raw["assembling-machine"]["assembling-machine-1"].crafting_categories,"organic-or-assembling")
 
 local do_extra_start_tweaks = false
 
